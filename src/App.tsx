fe84ef78350188c5c0f31e33bf89c77141b30d92
DmitryMolokanov <molod1690@yandex.ru>
2025-07-14 23:33:10 +0300

    feat: added restart break and round counted

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,87 @@
-import { useEffect, useMemo, useState } from 'react'
-import './App.css'
-import menu from './assets/icons/menu-burger.svg'
+import { useEffect, useMemo, useState } from "react";
+import "./App.css";
+import menu from "./assets/icons/menu-burger.svg";
+import bellSound from "./assets/sound/boxingBell.mp3";
 
 function App() {
-  const [min, setMin] = useState(3)
-  const [sec, setSec] = useState(0);
-  const [start, setStart] = useState(false)
-
+  const [initTimer, setInitTimer] = useState({ min: 3, sec: 0 });
+  const [min, setMin] = useState(0);
+  const [sec, setSec] = useState(5);
+  const [start, setStart] = useState(false);
+  const [round, setRound] = useState(1);
+  const [breakRound, setBreakRound] = useState(false);
 
   const startTimer = () => {
-    setStart(prev => !prev)
-  }
+    setStart((prev) => !prev);
+  };
 
+  const resetTimer = () => {
+    setStart(false);
+    setMin(initTimer.min);
+    setSec(initTimer.sec);
+  };
 
   useEffect(() => {
-    let timerSecId: number
-    if (start) {
+    let timerSecId: number;
+    if (start || breakRound) {
+      // гонг
+      if (initTimer.min === min && initTimer.sec === sec && start) {
+        const startBellSound = new Audio(bellSound);
+        startBellSound.play();
+      }
+      // декремент минут
       if (min > 0 && sec === 0) {
-        setMin(prev => prev - 1)
-        setSec(59)
+        setMin((prev) => prev - 1);
+        setSec(59);
       }
 
+      // декремент секунд
       if (sec > 0 && start) {
-        timerSecId = setTimeout(() => setSec(prev => prev - 1), 1000)
-        return () => clearTimeout(timerSecId)
+        timerSecId = setTimeout(() => setSec((prev) => prev - 1), 1000);
+        return () => clearTimeout(timerSecId);
       }
-      else {
-        return () => clearTimeout(timerSecId)
+
+      // перерыв
+      if (sec === 0 && min === 0) {
+        setBreakRound(true);
+        setMin(0);
+        setSec(3);
+      }
+
+      // следующий раунд
+      if (sec === 0 && min === 0 && breakRound) {
+        setBreakRound(false);
+        setStart(true);
+        setRound((prev) => prev + 1);
+        setMin(0);
+        setSec(5);
       }
     }
-  }, [start, sec, min]);
-
+  }, [start, sec, min, breakRound, initTimer]);
 
   const timeConverter = useMemo(() => {
-    return `${min.toString().padStart(2, '0')} : ${sec.toString().padStart(2, '0')}`
-  }, [min, sec])
+    return `${min.toString().padStart(2, "0")} : ${sec
+      .toString()
+      .padStart(2, "0")}`;
+  }, [min, sec]);
 
   return (
-    <div className='app-layout'>
-      <div className='header'>
-        <div className='header__menu-container'>
+    <div className="app-layout">
+      <div className="header">
+        <div className="header__menu-container">
           <img src={menu} alt="menu" />
         </div>
       </div>
-      <div className='content'>
-        <div className='timer'>
+      <div className="content">
+        <div className="timer">
+          <div className="timer__round-container">
+            <span>Round</span>
+            <span>{round}</span>
+          </div>
           <span>{timeConverter}</span>
         </div>
-        <div className='group-btn'>
-          <button>Reset</button>
+        <div className="group-btn">
+          <button onClick={resetTimer}>Reset</button>
           <button onClick={startTimer}>{!start ? "Start" : "Pause"}</button>
         </div>
       </div>
@@ -56,4 +89,4 @@ function App() {
   );
 }
 
-export default App
+export default App;
