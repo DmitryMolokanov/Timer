7a1d4d3856d7ea19c07bf5928e67c5b33ed5b383
PULSAR\molokanov <molokanov@pulsarm.ru>
2025-07-14 10:44:48 +0300

    feat: added header and menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,52 @@
-import { useCallback, useEffect, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import './App.css'
+import menu from './assets/icons/menu-burger.svg'
 
 function App() {
-  const [min, setMin] = useState(1)
-  const [sec, setSec] = useState(3);
+  const [min, setMin] = useState(3)
+  const [sec, setSec] = useState(0);
   const [start, setStart] = useState(false)
 
 
   const startTimer = () => {
-    setStart(true)
+    setStart(prev => !prev)
   }
 
-  const timer = useCallback(() => {
-
-    if (min > 0 && sec === 0) {
-      setMin(prev => prev - 1)
-      setSec(59)
-    }
-
-
-    if (sec > 0) {
-      const timerSecId = setTimeout(() => setSec(prev => prev - 1), 1000)
-      return () => clearTimeout(timerSecId)
-    }
-  }, [sec, min])
 
   useEffect(() => {
+    let timerSecId: number
     if (start) {
-      timer()
-    }
-  }, [timer, start]);
+      if (min > 0 && sec === 0) {
+        setMin(prev => prev - 1)
+        setSec(59)
+      }
 
+      if (sec > 0 && start) {
+        timerSecId = setTimeout(() => setSec(prev => prev - 1), 1000)
+        return () => clearTimeout(timerSecId)
+      }
+      else {
+        return () => clearTimeout(timerSecId)
+      }
+    }
+  }, [start, sec, min]);
+
+
+  const timeConverter = useMemo(() => {
+    return `${min.toString().padStart(2, '0')} : ${sec.toString().padStart(2, '0')}`
+  }, [min, sec])
 
   return (
-    <div>
-      <span>{min}:{sec}</span>
-      <button onClick={startTimer}>Start</button>
+    <div className='app-layout'>
+      <div className='header'>
+        <div className='header__menu-container'>
+          <img src={menu} alt="menu" />
+        </div>
+      </div>
+      <span>{timeConverter}</span>
+      <button onClick={startTimer}>{!start ? "Start" : "Pause"}</button>
     </div>
   );
-
 }
 
 export default App
