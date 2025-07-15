3623b2f9f591d23bc1e48ea4b4b3b2e1fb48dc88
PULSAR\molokanov <molokanov@pulsarm.ru>
2025-07-15 10:34:30 +0300

    refactor:refactored the core logic of timer

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,100 @@
-import { useEffect, useMemo, useState } from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import "./App.css";
 import menu from "./assets/icons/menu-burger.svg";
 import bellSound from "./assets/sound/boxingBell.mp3";
 
 function App() {
   const [initTimer, setInitTimer] = useState({ min: 3, sec: 0 });
-  const [min, setMin] = useState(0);
-  const [sec, setSec] = useState(5);
+  const [initBreakTimer, setInitBreackTimer] = useState({ min: 1, sec: 0 })
+  const [min, setMin] = useState(3);
+  const [sec, setSec] = useState(0);
   const [start, setStart] = useState(false);
-  const [round, setRound] = useState(1);
   const [breakRound, setBreakRound] = useState(false);
+  const [round, setRound] = useState(1);
+
+  const timerRef = useRef<number | undefined>(undefined)
 
   const startTimer = () => {
     setStart((prev) => !prev);
   };
 
-  const resetTimer = () => {
-    setStart(false);
+  const decrementMin = () => {
+    setMin((prev) => prev - 1);
+    setSec(59);
+  }
+
+  const decrementSec = () => {
+    timerRef.current = setTimeout(() => setSec((prev) => prev - 1), 1000);
+    return () => clearTimeout(timerRef.current);
+  }
+
+  const playSoundBell = () => {
+    const startBellSound = new Audio(bellSound);
+    return startBellSound.play();
+  }
+
+  const startBreak = useCallback(() => {
+    playSoundBell()
+    setBreakRound(true);
+    setMin(initBreakTimer.min);
+    setSec(initBreakTimer.sec);
+  }, [initBreakTimer])
+
+
+  const startNextRound = useCallback(() => {
+    playSoundBell()
+    setBreakRound(false);
+    setStart(true);
+    setRound((prev) => prev + 1);
     setMin(initTimer.min);
     setSec(initTimer.sec);
+  }, [initTimer])
+
+
+
+  const resetTimer = () => {
+    setStart(false);
+    setBreakRound(false)
+    setMin(initTimer.min);
+    setSec(initTimer.sec);
+    return clearTimeout(timerRef.current)
   };
 
+  const isTimerFinished = min === 0 && sec === 0
+
   useEffect(() => {
-    let timerSecId: number;
+
     if (start || breakRound) {
+
       // гонг
       if (initTimer.min === min && initTimer.sec === sec && start) {
-        const startBellSound = new Audio(bellSound);
-        startBellSound.play();
+        playSoundBell()
       }
+
       // декремент минут
       if (min > 0 && sec === 0) {
-        setMin((prev) => prev - 1);
-        setSec(59);
+        decrementMin()
+        return
       }
 
       // декремент секунд
       if (sec > 0 && start) {
-        timerSecId = setTimeout(() => setSec((prev) => prev - 1), 1000);
-        return () => clearTimeout(timerSecId);
+        decrementSec()
       }
 
-      // перерыв
-      if (sec === 0 && min === 0) {
-        setBreakRound(true);
-        setMin(0);
-        setSec(3);
-      }
-
-      // следующий раунд
-      if (sec === 0 && min === 0 && breakRound) {
-        setBreakRound(false);
-        setStart(true);
-        setRound((prev) => prev + 1);
-        setMin(0);
-        setSec(5);
+      // изменение начала раунда и отдыха
+      if (isTimerFinished) {
+        if (!breakRound) {
+          startBreak()
+        } else {
+          startNextRound()
+        }
       }
     }
-  }, [start, sec, min, breakRound, initTimer]);
+  }, [start, sec, min, breakRound, initTimer, isTimerFinished, startBreak, startNextRound]);
 
   const timeConverter = useMemo(() => {
-    return `${min.toString().padStart(2, "0")} : ${sec
-      .toString()
-      .padStart(2, "0")}`;
+    return `${min.toString().padStart(2, "0")} : ${sec.toString().padStart(2, "0")}`;
   }, [min, sec]);
 
   return (
