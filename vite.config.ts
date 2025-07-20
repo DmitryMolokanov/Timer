2326cc7c6a9ed8687042b1f21be024d396eb5f9d
DmitryMolokanov <molod1690@yandex.ru>
2025-07-20 16:14:25 +0300

    fix:update vitepwa

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     react(),
     VitePWA({
       workbox: {
-        globPatterns: ['**/*.{ts,tsx,css,html,svg, mp3}'],
+        globPatterns: ['**/*.{js,,css,html,svg, mp3}'],
         runtimeCaching: [
           {
             urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
